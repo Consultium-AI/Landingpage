1138390c799694931f42847224a411d94610ab73
Consultium-AI <info@consultiumai.nl>
2025-12-01 06:20:09 +0100

    Update address to Westerstraat 10, 3016DH Rotterdam, Nederland

--- src/App.jsx
+++ src/App.jsx
@@ -1356,7 +1356,7 @@ Daarom bouwen we intuïtieve AI-tools die moeiteloos meedraaien in het dagelijks
                       <svg className="w-4 h-4 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
                         <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"/>
                       </svg>
-                      <span>Jan Pietersz. Coenstraat 7<br />2595WP 's-Gravenhage<br />Den Haag</span>
+                      <span>Westerstraat 10<br />3016DH Rotterdam<br />Nederland</span>
                     </li>
                     <li className="flex items-center gap-2">
                       <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
